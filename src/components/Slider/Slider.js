3b4eafcedf8fd3ff518f4f10627b6d83d5a3c92f
Suhendar <suhendararyadi@gmail.com>
2024-12-07 00:21:13 +0700

    tambah slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -37,92 +37,16 @@ const Slider = () => {
     <section id="slider-part" className="slider-active">
       <div 
         className="single-slider bg_cover pt-150" 
-        style={{ backgroundImage: "url('/images/slider/s-1.jpg')" }} 
-      >
+        style={{ backgroundImage: "url('/images/slider/slider-iaipi-1.jpg')" }} 
+        >
         <div className="container">
-          <div className="row">
-            <div className="col-xl-7 col-lg-9">
-              <div className="slider-cont">
-                <h1>Choose the right theme for education</h1>
-                <p>
-                  Donec vitae sapien ut libearo venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt Sed fringilla mauri amet nibh.
-                </p>
-                <ul>
-                  <li>
-                    <Link className="main-btn" href="#">
-                      Read More
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="main-btn main-btn-2" href="#">
-                      Get Started
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
 
       <div 
         className="single-slider bg_cover pt-150" 
-        style={{ backgroundImage: "url('/images/slider/s-2.jpg')" }} 
-      >
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-7 col-lg-9">
-              <div className="slider-cont">
-                <h1>Choose the right theme for education</h1>
-                <p>
-                  Donec vitae sapien ut libearo venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt Sed fringilla mauri amet nibh.
-                </p>
-                <ul>
-                  <li>
-                    <Link className="main-btn" href="#">
-                      Read More
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="main-btn main-btn-2" href="#">
-                      Get Started
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div 
-        className="single-slider bg_cover pt-150" 
-        style={{ backgroundImage: "url('/images/slider/s-3.jpg')" }} 
-      >
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-7 col-lg-9">
-              <div className="slider-cont">
-                <h1>Choose the right theme for education</h1>
-                <p>
-                  Donec vitae sapien ut libearo venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt Sed fringilla mauri amet nibh.
-                </p>
-                <ul>
-                  <li>
-                    <Link className="main-btn" href="#">
-                      Read More
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="main-btn main-btn-2" href="#">
-                      Get Started
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+        style={{ backgroundImage: "url('/images/slider/slider-iaipi-4.jpg')" }} 
+        >
       </div>
     </section>
   )
