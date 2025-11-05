1bb9c9fa5dccb33c3d5cd2947d6a39df73f1bc5a
Suhendar <suhendararyadi@gmail.com>
2025-11-05 13:21:37 +0700

    feat: Update icon for Program Studi Ilmu Al-Quran dan Tafsir to file text

--- src/data/lembagaData.js
+++ src/data/lembagaData.js
@@ -138,7 +138,7 @@ const lembagaData = [
     fullName: "Program Studi Ilmu Al-Quran dan Tafsir",
     description: "Berfokus pada pengkajian dan penelitian Al-Quran sebagai sumber utama ajaran Islam dengan kajian tafsir kontemporer.",
     url: "/fakultas/ushuluddin/iat",
-    icon: "fa-bookmark",
+    icon: "fa-file-text-o",
     category: "Prodi",
     fakultas: "Ushuluddin",
     status: "active"
